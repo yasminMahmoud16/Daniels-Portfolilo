a767e8cdc5d110a49d4d3687a6c207c9ff30e823
yasminMahmoud16 <153022052+yasminMahmoud16@users.noreply.github.com>
2024-11-18 14:30:48 +0200

    Add files via upload

---
+++ Danils-Portfolio/js/main.js
@@ -0,0 +1,16 @@
+
+// typed animation
+
+var typed = new Typed('#typed-txt', {
+    strings: ['Designer', 'Larry Daniels', 'Developer'],
+    typeSpeed: 100,
+    backSpeed: 50,        
+    backDelay: 500,       
+    startDelay: 300,
+    loop: true,
+    html: true
+});
+
+
+
+
